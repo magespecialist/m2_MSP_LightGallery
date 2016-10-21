63b80e2944629976974f218e103b308ba1858074
Lorenzo Stramaccia <stramaccia@idealiagroup.com>
2016-10-21 12:03:38 +0200

    Inital commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,30 @@
+
+/**
+ * IDEALIAGroup srl
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@idealiagroup.com so we can send you a copy immediately.
+ *
+ * @category MSP
+ * @package LightGallery
+ * @copyright Copyright (c) 2016 IDEALIAGroup srl (http://www.idealiagroup.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+var config = {
+    "map": {
+        "*": {
+            "LightGallery": "MSP_LightGallery/js/lightgallery"
+        }
+    },
+    "shim":{
+        "MSP_LightGallery/js/lightgallery.min": ["jquery"]
+    }
+};
